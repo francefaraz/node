c579d44b5ff41593dcc8746fc999a101be12c527
skmuneer <muneer@nuevesolutions.com>
2022-04-09 19:08:37 +0530

    tdd passed implemented api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,63 @@
 const User = require('../schema/user.schema');
+// const Post= require('../schema/post.schema');
+
+const aggregate =  User.aggregate([     
+    {
+      $lookup: {
+        from: 'posts',
+        localField: '_id',
+        foreignField: 'userId',
+        as: 'posts',
+      },
+    },
+    {
+        $project: {
+          name:'$name',
+          posts: { $size: '$posts' },
+          
+        },
+      },
+  ]);
 
 module.exports.getUsersWithPostCount = async (req, res) => {
+    
     try {
-        //TODO: Implement this API
-        res.status(200).json({
-            message: 'Implement this API'
-        })
-    } catch (error) {
-        res.send({error: error.message});
-    }
-}
+        var page=req.query.page || 1;
+        console.log(page)
+          const options = {
+            page: parseInt(page),
+            limit: 10,
+           
+        };
+        
+        User.aggregatePaginate(aggregate,options).then((data) => {
+            res.send({
+                "data":{
+
+              users: data.docs,
+              "pagination":{
+                "totalDocs": data.totalDocs,
+                "limit": data.limit,
+                "page": data.page,
+                "totalPages": data.totalPages,
+                "pagingCounter": data.pagingCounter,
+                "hasPrevPage": data.hasPrevPage,
+                "hasNextPage": data.hasNextPage,
+                "prevPage": data.prevPage,
+                "nextPage": data.nextPage,
+              }
+                }
+            });
+          })
+          .catch((err) => {
+            res.status(500).send({
+              message:
+                err.message || "Some error occurred while retrieving data.",
+            });});
+        } catch (error) {
+          res.status(404).json({
+            status: 'fail',
+            message: error,
+          });
+        }
+      };
--- src/schema/user.schema.js
+++ src/schema/user.schema.js
@@ -4,5 +4,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
     name: String,
 });
+var aggregatePaginate = require('mongoose-aggregate-paginate-v2');
+userSchema.plugin(aggregatePaginate)
 
 module.exports = mongoose.model('User', userSchema);
